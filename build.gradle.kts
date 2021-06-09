73f7a30192660300bd2a035dc89bcd6fa93a03f7
dyusupov <dmitry.yusupov@infobip.com>
2021-06-09 17:02:18 +0300

    step-7-separate-applications Removed boot jar task from root gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,55 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.5.0"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.10"
-	kotlin("plugin.spring") version "1.5.10"
-	kotlin("plugin.jpa") version "1.5.10"
-	kotlin("plugin.allopen") version "1.3.61"
+    id("org.springframework.boot") version "2.5.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.10"
+    kotlin("plugin.spring") version "1.5.10"
+    kotlin("plugin.jpa") version "1.5.10"
+    kotlin("plugin.allopen") version "1.3.61"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_14
 
 allprojects {
-	group = "ru.yusdm.monolithtomicro"
-	version = "0.0.1-SNAPSHOT"
+    group = "ru.yusdm.monolithtomicro"
+    version = "0.0.1-SNAPSHOT"
 
-	repositories {
-		mavenCentral()
-	}
+    repositories {
+        mavenCentral()
+    }
 
-	tasks.withType<Test> {
-		useJUnitPlatform()
-	}
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 
-	tasks.withType<JavaCompile> {
-		sourceCompatibility = "14"
-		targetCompatibility = "14"
-	}
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "14"
+        targetCompatibility = "14"
+    }
 
-	tasks.withType<KotlinCompile> {
-		kotlinOptions {
-			freeCompilerArgs = listOf("-Xjsr305=strict")
-			jvmTarget = "14"
-		}
-	}
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "14"
+        }
+    }
 
 }
 
 subprojects {
-	repositories {
-		mavenCentral()
-	}
-	apply {
-		plugin("org.jetbrains.kotlin.jvm")
-	}
+    repositories {
+        mavenCentral()
+    }
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+    }
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
+}
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
 }
